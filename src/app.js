7084b213cd8262ac7d108f1a1e1e4dac3bb326bd
willymiller <65316037+willymiller@users.noreply.github.com>
2022-02-17 21:13:09 -0300

    Modificando home y styles

--- src/app.js
+++ src/app.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const homeRoutes= require('./routes/home')
-const productDetailRoutes=require('./routes/productDetail')
+const webRoutes = require('./routes/webRoutes');
+
+/* const homeRoutes= require('./routes/home')
+const productDetailRoutes=require('./routes/productDetail') */
 
 app.set('view engine', 'ejs');
-app.get("/", homeRoutes);
-app.get("/product", productDetailRoutes);
+/* app.get("/", homeRoutes);
+app.get("/product", productDetailRoutes); */
+
+app.use('/',webRoutes)
 
 app.use(express.static("public"));
 
--- src/routes/webRoutes.js
+++ src/routes/webRoutes.js
@@ -1,7 +1,10 @@
 const express = require ('express')
 const router = express.Router()
+const homeControllers = require ('../controllers/homeControllers.js')
 const productDetailControllers = require ('../controllers/productControllers.js')
 
-router.get('/detailProduct', productDetailControllers.detail)
+router.get('/', homeControllers.index)
+
+router.get('/product', productDetailControllers.detail)
 
 module.exports=router
